a9936756a280f9a67808e6718d496217ffe42a83
Zainalithegreat <zain.alithe.hi@gmail.com>
2025-06-09 03:51:31 -0700

    fixes

--- backend/server/server.js
+++ backend/server/server.js
@@ -8,7 +8,7 @@ import fs from 'fs';
 const connection =  {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
+    server: process.env.DB_HOST,
     database: process.env.DB_NAME
 };
 
