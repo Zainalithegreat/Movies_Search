4e0f07f23208395099258fcdcad6c625c8dc50c5
Zainalithegreat <zain.alithe.hi@gmail.com>
2025-06-09 05:42:47 -0700

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.'   // ✅ puts it in the root of dist/
-        }
-      ]
-    })
   ]
 });
